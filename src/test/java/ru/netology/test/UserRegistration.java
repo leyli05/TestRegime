892e54bd76d4164118a0253d930617936663bf34
leily <leily@example.com>
2024-03-10 11:33:00 +0300

    Chamge tests

---
+++ src/test/java/ru/netology/test/UserRegistration.java
@@ -0,0 +1,61 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.UserGenerator;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.UserGenerator.*;
+
+class UserRegistration {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldLoginActiveUser() {
+        UserInfo user = UserGenerator.Registration.generateUser("active");
+        activeUser(user);
+        $(".input__box>[type='text']").setValue(user.getLogin());
+        $(".input__box>[type='password']").setValue(user.getPassword());
+        $(".button").click();
+        $(withText("Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldLoginBlockedUser() {
+        UserInfo user = UserGenerator.Registration.generateUser("blocked");
+        blockedUser(user);
+        $(".input__box>[type='text']").setValue(user.getLogin());
+        $(".input__box>[type='password']").setValue(user.getPassword());
+        $(".button").click();
+        $(withText("Пользователь заблокирован")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldWrongLogin() {
+        UserInfo user = Registration.generateUser("active");
+        activeUser(user);
+        var anotherLogin = generateLogin();
+        $(".input__box>[type='text']").setValue(anotherLogin);
+        $(".input__box>[type='password']").setValue(user.getPassword());
+        $(".button").click();
+        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldWrongPassword() {
+        UserInfo user = Registration.generateUser("active");
+        activeUser(user);
+        var anotherPassword = generatePassword();
+        $(".input__box>[type='text']").setValue(user.getLogin());
+        $(".input__box>[type='password']").setValue(anotherPassword);
+        $(".button").click();
+        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible);
+    }
+}
